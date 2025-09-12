STMicroelectronics/u-boot

commit bc9071c9f318aa69c815927b3828096d1976de1b

usb: gadget: fastboot: Dequeue the previous IN request for the current request
Recent versions of the fastboot tool will query the partition type before doing
an operation on a partition (such as erase, flash, etc). It will then submit
the operation as soon as the response for the partition type is received.

Usually, the MUSB controller will see that the partition type request return
status was read by the host at the very same time as the actual operation
request is submitted by the host. However, the operation will be read first
(int_rx is handled first in musb_interrupt) and after it is completed, the
fastboot USB gadget driver will send another return status. Hence, this happens
before the musb gadget framework has had a chance to handle the previous
acknowledgement that the host read the return status and dequeue the request.

The host will then usually empty the FIFO by the time musb_interrupt gets around
handling the return status acknowledgement (for the previous request, this is
still on the same musb_interrupt call), so no other interrupt is generated and
the most recent return status acknowledgement remains unaccounted for.

It will then be used as a response for the next command, and the proper response
for it will be delayed to the next command, and so on.

Dequeuing the previous IN request in the fastboot code ensures that no previous
return status remains. It is acceptable to do it since there is no callback to
it anyways.

Signed-off-by: Paul Kocialkowski <contact@paulk.fr>

drivers/usb/gadget/f_fastboot.c
@@ -311,6 +311,9 @@ static int fastboot_tx_write(const char *buffer, unsigned int buffer_size)
 
 	memcpy(in_req->buf, buffer, buffer_size);
 	in_req->length = buffer_size;
+
+	usb_ep_dequeue(fastboot_func->in_ep, in_req);
+
 	ret = usb_ep_queue(fastboot_func->in_ep, in_req, 0);
 	if (ret)
 		printf("Error %d on queue\n", ret);

commit 86b1e4cf455cba305ca38b2a2c1eed970217fc45

fastboot: only dequeue IN requests that has been queued
Everytime a TX is needed, usb_ep_dequeue() is called unconditionally
for the IN request. When no request has been queued, or the request
has completed, this results in below error logs to be printed by the
DWC3 controller driver [1]:

dwc3-generic-peripheral usb@... request... was not queued to ep1in-bulk

It seems harmless, still fix it by keeping track of the IN request
being queued, to only dequeue it when needed.

Note: there's no need to keep track when the fastboot command complete
ends up in a detach or reboot/boot (e.g. compl_do_reset(),
do_exit_on_complete(), do_bootm_on_complete()). Only take care of
fastboot_complete().

In addition, in case a request has been queued, the usb_ep_dequeue() may
do some cache, e.g. unmap operations (see dwc3/gadget.c). That seems
odd, to fill in the "in_req->buf", with new data, before it gets
unmap'ed from the controller.
So move the usb_ep_dequeue() before the memcpy(in_req->buf, ...).

[1] https://lists.denx.de/pipermail/u-boot/2023-September/530612.html

Fixes: bc9071c9f318 ("usb: gadget: fastboot: Dequeue the previous IN request for the current request")
Change-Id: I6ec23fae843a6e39e85a53d62f614b2444bed647
Signed-off-by: Fabrice Gasnier <fabrice.gasnier@foss.st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/471147
Domain-Review: Patrice CHOTARD <patrice.chotard@foss.st.com>
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Reviewed-by: Patrice CHOTARD <patrice.chotard@foss.st.com>

drivers/usb/gadget/f_fastboot.c
@@ -44,6 +44,7 @@ struct f_fastboot {
 	/* IN/OUT EP's and corresponding requests */
 	struct usb_ep *in_ep, *out_ep;
 	struct usb_request *in_req, *out_req;
+	bool in_req_queued;
 };
 
 static char fb_ext_prop_name[] = "DeviceInterfaceGUID";
@@ -198,8 +199,10 @@ static void rx_handler_command(struct usb_ep *ep, struct usb_request *req);
 static void fastboot_complete(struct usb_ep *ep, struct usb_request *req)
 {
 	int status = req->status;
-	if (!status)
+	if (!status) {
+		fastboot_func->in_req_queued = false;
 		return;
+	}
 	printf("status: %d ep '%s' trans: %d\n", status, ep->name, req->actual);
 }
 
@@ -403,14 +406,20 @@ static int fastboot_tx_write(const char *buffer, unsigned int buffer_size)
 	struct usb_request *in_req = fastboot_func->in_req;
 	int ret;
 
+	if (fastboot_func->in_req_queued) {
+		usb_ep_dequeue(fastboot_func->in_ep, in_req);
+		fastboot_func->in_req_queued = false;
+	}
+
 	memcpy(in_req->buf, buffer, buffer_size);
 	in_req->length = buffer_size;
-
-	usb_ep_dequeue(fastboot_func->in_ep, in_req);
 
 	ret = usb_ep_queue(fastboot_func->in_ep, in_req, 0);
 	if (ret)
 		printf("Error %d on queue\n", ret);
+	else
+		fastboot_func->in_req_queued = true;
+
 	return 0;
 }
 